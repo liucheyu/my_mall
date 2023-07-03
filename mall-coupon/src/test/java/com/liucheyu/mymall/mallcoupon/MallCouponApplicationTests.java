57e1bd597453536d273a8a0598347d816a8395d8
tetsu <liucheyu1987@gmail.com>
2023-07-03 17:18:03 +0800

    add modules

---
+++ mall-coupon/src/test/java/com/liucheyu/mymall/mallcoupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liucheyu.mymall.mallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
